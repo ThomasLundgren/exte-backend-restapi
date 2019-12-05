0fa676850156eeeec6d9014f4bc5b6c42b9a28c3
Thomas Lundgren <thomasLundgren@live.com>
2019-12-05 16:47:00 +0100

    Added Javadoc for SubjectRepository

--- src/main/java/se/hig/exte/repository/SubjectRepository.java
+++ src/main/java/se/hig/exte/repository/SubjectRepository.java
@@ -3,18 +3,31 @@ package se.hig.exte.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import se.hig.exte.model.Academy;
+import se.hig.exte.model.Exam;
 import se.hig.exte.model.Subject;
 
+/**
+ * An interface used for database operations on {@link Exam} objects.
+ * References to this interface are automatically given an implementation by Spring Boot when using
+ * the {@code Autowired} annotation.
+ */
+@Repository
 public interface SubjectRepository extends JpaRepository<Subject, Integer>{
-	
-	/*
-	 * The Repository interface is the Spring equivalent of DAO classes.
+
+	/**
+	 * Fetches the {@link Subject} object with the given ID from the database.
+	 * @param id The ID of the {@link Subject} object to fetch.
+	 * @return The {@link Exam} object representing the database record with the specified ID.
 	 */
+	Subject findById(int id);
 	
-	List<Subject> findByName(String name);
-	
-	Subject findById(int name);
-	
+	/**
+	 * Fetches all {@link Subject} objects which belong to the specified {@link Academy} from the database.
+	 * @param academyId The ID of the {@link Academy}.
+	 * @return A {@link List} containing all {@link Subject}s belonging to the {@link Academy} with the specified ID.
+	 */
 	List<Subject> findByAcademyId(int academyId);
 }
