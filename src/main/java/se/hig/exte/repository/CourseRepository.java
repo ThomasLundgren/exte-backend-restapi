38c36796fd79dc26e8e4552776623ab9f191e376
Thomas Lundgren <thomasLundgren@live.com>
2019-12-05 16:38:25 +0100

    Added Javadoc for CourseRepository

--- src/main/java/se/hig/exte/repository/CourseRepository.java
+++ src/main/java/se/hig/exte/repository/CourseRepository.java
@@ -7,12 +7,25 @@ import org.springframework.stereotype.Repository;
 
 import se.hig.exte.model.Course;
 
+/**
+ * An interface used for database operations on {@link Course} objects.
+ * References to this interface are automatically given an implementation by Spring Boot when using
+ * the {@code Autowired} annotation.
+ */
 @Repository
 public interface CourseRepository extends JpaRepository<Course, Integer> {
 
+	/**
+	 * Fetches the {@link Course} object with the given ID from the database.
+	 * @param id The ID of the {@link Course} object to fetch.
+	 * @return The {@link Course} object representing the database record with the specified ID.
+	 */
 	Course findById(int id);
 
-	List<Course> findByCourseCode(String courseCode);
-
+	/**
+	 * Fetches all {@link Course} objects which belong to the specified {@link Subject} from the database.
+	 * @param subjectId The ID of the {@link Subject}.
+	 * @return A {@link List} containing all {@link Course}s belonging to the {@link Subject} with the specified ID.
+	 */
 	List<Course> findBySubjectId(int subjectId);
 }
