6fd0f16065ef0c4021e07135e44ae7aa47ad7bd1
SannaLundqvist <lundqvist174@gmail.com>
2019-12-05 15:13:15 +0100

    qfix

--- src/main/java/se/hig/exte/controller/SubjectController.java
+++ src/main/java/se/hig/exte/controller/SubjectController.java
@@ -1,10 +1,13 @@
 package se.hig.exte.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -46,7 +49,7 @@ public class SubjectController {
 		subjectService.deleteById(id);
 	}
 
-	@PostMapping("/")
+	@PatchMapping("/")
 	public ResponseEntity<Subject> patchSubject(@RequestBody Subject subject) {
 		Subject patchedSubject = subjectService.save(subject);
 		return new ResponseEntity<Subject>(patchedSubject, HttpStatus.OK);
