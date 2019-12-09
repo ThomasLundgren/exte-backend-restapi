d48cd39efd73e47846c0d82419fe9e22951ba42e
SannaLundqvist <lundqvist174@gmail.com>
2019-12-09 12:13:45 +0100

    grunden för login
    Dock saknar obejkt istället för / i URL än så länge, lunchpaus på det
    Jag har okså glömt mitt lösenord så det är mycket svårt att testa logga in
    En Dummy är tyvärr kvar för att slippa testa med URL

---
+++ src/main/java/se/hig/exte/restcontroller/LoginController.java
@@ -0,0 +1,33 @@
+package se.hig.exte.restcontroller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import se.hig.exte.model.Exam;
+import se.hig.exte.service.LoginService;
+
+@RestController
+@RequestMapping("/login")
+public class LoginController {
+	private LoginService loginService;
+	
+	@Autowired
+	public LoginController(LoginService loginService) {
+		this.loginService = loginService;
+	}
+	/*
+	@GetMapping("/{username}/{password}")
+	public boolean loginAdmin(@PathVariable String username, @PathVariable String password) {
+		return (loginService.login(username, password));
+	}
+	*/
+	@GetMapping("/")
+	public boolean loginAdmin() {
+		return (loginService.login("hej", "då"));
+	}
+}
--- src/main/java/se/hig/exte/service/LoginService.java
+++ src/main/java/se/hig/exte/service/LoginService.java
@@ -1,20 +1,37 @@
 package se.hig.exte.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import login.ILoginHandler;
 import login.LoginHandler;
 
+@Service
 public class LoginService {
 	private final ILoginHandler loginHandler;
+	@Autowired
+	private final UserService userService;
 	
 	/**
 	 * @param loginHandler
 	 */
 	
-	public LoginService() {
+	public LoginService(UserService userService) {
+		this.userService = userService;
 		this.loginHandler = new LoginHandler();
 	}
-	public void login(String username, String password) {
-		
+	public boolean login(String username, String password) {
+		System.out.println("Trying to log in...");
+		boolean isLoggedIn = false;
+		if(checkIfUserExists(username)) {
+			System.out.println("user exists in db...");
+			isLoggedIn = loginHandler.login(username, password);
+		}
+		return isLoggedIn;
+	}
+	
+	private boolean checkIfUserExists(String username) {
+		return !userService.findByName(username).isEmpty();
 	}
 	
 }
