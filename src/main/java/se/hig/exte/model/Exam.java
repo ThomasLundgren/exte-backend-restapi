12de6b57b9aed248d8afccc8cc91fa3e84d27a3c
NiklasNordgren <39832486+NiklasNordgren@users.noreply.github.com>
2019-12-16 18:13:50 +0100

    Update Exam.java

--- src/main/java/se/hig/exte/model/Exam.java
+++ src/main/java/se/hig/exte/model/Exam.java
@@ -1,6 +1,5 @@
 package se.hig.exte.model;
 
-import java.net.URL;
 import java.time.LocalDate;
 
 import javax.persistence.Entity;
@@ -12,8 +11,8 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 /**
- * A model/entity class that represents an Exam. The fields of this class maps to columns in the
- * database.
+ * A model/entity class that represents an Exam. The fields of this class maps
+ * to columns in the database.
  */
 @Entity
 public class Exam {
@@ -23,44 +22,44 @@ public class Exam {
 	private int id;
 	@NotBlank(message = "Name cannot be blank, empty or null")
 	@Size(min = 2, message = "Name must be at least two characters long")
-	private String name;
+	private String fileName;
 	@NotNull(message = "Date cannot be null")
 	private LocalDate date;
 	@NotNull(message = "UnpublishDate cannot be null")
 	private LocalDate unpublishDate;
-	@NotNull(message = "PdfUrl cannot be null")
-	private URL pdfUrl; /* 
-							* address to FTP-resource on the form:
-							* scheme:[//authority][/path][?query][#fragment]
-							* Example: 
-							* ftp://theuser:thepassword@someauthority:21
-							*/
 	private boolean unpublished;
 	private int courseId;
-	
+
 	// Only used for JPA/Spring, which is why it is declared with protected.
-	protected Exam() {}
-	
+	protected Exam() {
+	}
+
 	/**
 	 * Creates an {@code Exam} object.
-	 * @param name The name of the {@code Exam}. Must be at least two characters long and cannot be null.
-	 * @param date The date of the {@code Exam}. Cannot be null.
-	 * @param unpublishDate The date at which the {@code Exam} should be unpublished from the website. Cannot be null.
-	 * @param pdfUrl The URL to the PDF file that corresponds to an {@code Exam}. Cannot be null.
-	 * @param unpublished Set to true if this {@code Exam} should NOT be published on the website, otherwise false.
-	 * @param courseId The ID of the {@link Course} to which an {@code Exam} belongs. Cannot be null.
+	 * 
+	 * @param name          The name of the {@code Exam}. Must be at least two
+	 *                      characters long and cannot be null.
+	 * @param date          The date of the {@code Exam}. Cannot be null.
+	 * @param unpublishDate The date at which the {@code Exam} should be unpublished
+	 *                      from the website. Cannot be null.
+	 * @param pdfUrl        The URL to the PDF file that corresponds to an
+	 *                      {@code Exam}. Cannot be null.
+	 * @param unpublished   Set to true if this {@code Exam} should NOT be published
+	 *                      on the website, otherwise false.
+	 * @param courseId      The ID of the {@link Course} to which an {@code Exam}
+	 *                      belongs. Cannot be null.
 	 */
-	public Exam(String name, LocalDate date, LocalDate unpublishDate, URL pdfUrl, boolean unpublished, int courseId) {
-		this.name = name;
+	public Exam(String fileName, LocalDate date, LocalDate unpublishDate, boolean unpublished, int courseId) {
+		this.fileName = fileName;
 		this.date = date;
 		this.unpublishDate = unpublishDate;
-		this.pdfUrl = pdfUrl;
 		this.unpublished = unpublished;
 		this.courseId = courseId;
 	}
 
 	/**
 	 * Get the ID of this {@code Exam}.
+	 * 
 	 * @return The ID of this {@code Exam}.
 	 */
 	public int getId() {
@@ -69,54 +68,53 @@ public class Exam {
 
 	/**
 	 * Get the name of this {@code Exam}.
+	 * 
 	 * @return The name of this {@code Exam}.
 	 */
-	public String getName() {
-		return name;
+	public String getFileName() {
+		return fileName;
 	}
 
 	/**
 	 * Get the date of this {@code Exam}.
+	 * 
 	 * @return The date of this {@code Exam}.
 	 */
 	public LocalDate getDate() {
 		return date;
 	}
-	
+
 	/**
 	 * Get the date at which this {@code Exam} will be unpublished from the website.
+	 * 
 	 * @return The unpublish date of this {@code Exam}.
 	 */
 	public LocalDate getUnpublishDate() {
 		return unpublishDate;
 	}
-	
-	/**
-	 * Get the URL of the PDF file to which this {@code Exam} corresponds.
-	 * @return The URL of the PDF file to which this {@code Exam} corresponds.
-	 */
-	public URL getPdfUrl() {
-		return pdfUrl;
-	}
 
 	/**
 	 * Get the {@link Course} to which this {@code Exam} belongs.
+	 * 
 	 * @return The ID of the {@link Course} to which this {@code Exam} belongs.
 	 */
 	public int getCourseId() {
 		return courseId;
 	}
-	
+
 	/**
-	 * Set the name of this {@code Exam}. Must be at least two characters long and not null.
+	 * Set the name of this {@code Exam}. Must be at least two characters long and
+	 * not null.
+	 * 
 	 * @param name The new name of this {@code Exam}.
 	 */
-	public void setName(String name) {
-		this.name = name;
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
 	}
 
 	/**
 	 * Set the date of this {@code Exam}. Cannot be null.
+	 * 
 	 * @param date The new date of this {@code Exam}.
 	 */
 	public void setDate(LocalDate date) {
@@ -125,6 +123,7 @@ public class Exam {
 
 	/**
 	 * Set the unpublish date of this {@code Exam}. Cannot be null.
+	 * 
 	 * @param unpublishDate The new unpublish date of this {@code Exam}.
 	 */
 	public void setUnpublishDate(LocalDate unpublishDate) {
@@ -132,19 +131,11 @@ public class Exam {
 	}
 
 	/**
-	 * Set the PDF file URL that corresponds to this {@code Exam}. Cannot be null.
-	 * @param pdfUrl The new PDF file URL that corresponds to this {@code Exam}.
-	 */
-	public void setPdfUrl(URL pdfUrl) {
-		this.pdfUrl = pdfUrl;
-	}
-
-	/**
 	 * {@inheritDoc}
 	 */
 	@Override
 	public String toString() {
-		return "name: " + name + "\nid: " + id + "\nDate: " + date + "\nunpublishDate: " + unpublishDate + "\nURL: " + pdfUrl
+		return "fileName" + fileName + "\nid: " + id + "\nDate: " + date + "\nunpublishDate: " + unpublishDate
 				+ "\nUnpublished: " + unpublished;
 	}
 
@@ -156,5 +147,4 @@ public class Exam {
 		this.unpublished = unpublished;
 	}
 
-	
 }
