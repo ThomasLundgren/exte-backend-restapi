4b2e298dcecb700f930faaa794b8aa1c83be82b9
Thomas Lundgren <thomasLundgren@live.com>
2020-01-16 11:33:22 +0100

    Update AcademyController.java

--- src/main/java/se/hig/exte/restcontroller/AcademyController.java
+++ src/main/java/se/hig/exte/restcontroller/AcademyController.java
@@ -17,7 +17,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import se.hig.exte.model.Academy;
-import se.hig.exte.model.Course;
 import se.hig.exte.model.Subject;
 import se.hig.exte.service.AcademyService;
 import se.hig.exte.service.CookieHandler;
@@ -126,28 +125,31 @@ public class AcademyController {
 	}
 
 	/**
-	 * Changes the boolean unpublished value on the {@link Academy} 
-	 * @param academy The {@link Academy} to update 
+	 * Changes the boolean unpublished value on the {@link Academy}
+	 * 
+	 * @param academy The {@link Academy} to update
 	 * @return The ResponseEntity string of the http status.
 	 */
 	@PostMapping("/unpublish")
 	public ResponseEntity<Academy> unpublishAcademy(@RequestBody Academy academy, HttpServletRequest request) {
-		if(CookieHandler.isValidSuperSession(request.getCookies()))
+		if (cookieHandler.isValidSuperSession(request.getCookies()))
 			return new ResponseEntity<Academy>(unpublishService.setAcademyUnpublished(academy), HttpStatus.OK);
 		else
 			return new ResponseEntity<Academy>(HttpStatus.UNAUTHORIZED);
 	}
-	
+
 	/**
-	 * Changes the boolean unpublished value on the {@link Academy} 
-	 * @param subject The {@link Subject} to update 
+	 * Changes the boolean unpublished value on the {@link Academy}
+	 * 
+	 * @param subject     The {@link Subject} to update
 	 * @param unpublished The boolean is unpublished
 	 * @return The ResponseEntity string of the http status.
 	 */
 	@PostMapping("/unpublish/{unpublished}")
-	public ResponseEntity<String> unpublishAcademy(@RequestBody List<Academy> academies, @PathVariable boolean unpublished, HttpServletRequest request) {
-		if(cookieHandler.isValidSuperSession(request.getCookies()))
-			return unpublishService.setAcademiesUnpublished(academies, unpublished);	
+	public ResponseEntity<String> unpublishAcademy(@RequestBody List<Academy> academies,
+			@PathVariable boolean unpublished, HttpServletRequest request) {
+		if (cookieHandler.isValidSuperSession(request.getCookies()))
+			return unpublishService.setAcademiesUnpublished(academies, unpublished);
 		else
 			return new ResponseEntity<String>(HttpStatus.UNAUTHORIZED);
 	}
