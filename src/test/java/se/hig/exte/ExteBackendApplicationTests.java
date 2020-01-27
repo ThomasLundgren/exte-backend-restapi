31f6d1ab10dcd378c464d579d856089fba81041c
NiklasNordgren <39832486+NiklasNordgren@users.noreply.github.com>
2020-01-27 21:48:08 +0100

    Update ExteBackendApplicationTests.java

--- src/test/java/se/hig/exte/ExteBackendApplicationTests.java
+++ src/test/java/se/hig/exte/ExteBackendApplicationTests.java
@@ -1,13 +1,8 @@
 package se.hig.exte;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ExteBackendApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
