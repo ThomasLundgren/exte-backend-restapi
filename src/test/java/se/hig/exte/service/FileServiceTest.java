3a3046b4ae4bee38dc5db22533aa18fdaf70c285
NiklasNordgren <39832486+NiklasNordgren@users.noreply.github.com>
2020-01-27 19:01:11 +0100

    Update FileServiceTest.java

--- src/test/java/se/hig/exte/service/FileServiceTest.java
+++ src/test/java/se/hig/exte/service/FileServiceTest.java
@@ -1,14 +1,5 @@
 package se.hig.exte.service;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-import org.junit.jupiter.api.Test;
-
 class FileServiceTest {
-
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
 
 }
