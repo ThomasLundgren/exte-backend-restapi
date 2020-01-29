4691f543f3d3570312c4a5be5c0468cbb3e2cc4e
SannaLundqvist <lundqvist174@gmail.com>
2020-01-29 13:24:39 +0100

    new testitest

--- src/main/java/se/hig/exte/service/LoginService.java
+++ src/main/java/se/hig/exte/service/LoginService.java
@@ -1,10 +1,13 @@
 package se.hig.exte.service;
 
 import java.security.NoSuchAlgorithmException;
+import java.util.HashMap;
 
 import javax.servlet.http.Cookie;
 
+import org.apache.tomcat.jni.Time;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.http.ResponseCookie;
 import org.springframework.stereotype.Service;
 
@@ -16,8 +19,11 @@ public class LoginService {
 	private final ILoginHandler loginHandler;
 	private final UserService userService;
 	private final CookieHandler cookieHandler;
+	private HashMap<String, Integer> failedLogins;
+	private final int MAX_LOGIN_FAILS = 3;
 
 	/**
+	 * 
 	 * @param loginHandler
 	 */
 	@Autowired
@@ -25,18 +31,41 @@ public class LoginService {
 		this.userService = userService;
 		this.cookieHandler = cookieHandler;
 		loginHandler = new LoginHandler();
+		this.failedLogins = new HashMap<String, Integer>();
 	}
 
 	public ResponseCookie login(String username, String password) {
-//		ResponseCookie cookie = null;
-//		if (checkIfUserExists(username)) {
-//			boolean isLoggedIn = loginHandler.login(username.toString(), password.toString());
-//			if (isLoggedIn) {
-//				cookie = createCookie(userService.findByName(username).get(0).isSuperUser());
-//			}
-//		}
-//		return cookie;
-		return createCookie(userService.findByName(username).get(0).isSuperUser());
+		ResponseCookie cookie = null;
+		if (isAllowedToTryToLogin(username)) {
+			boolean isLoggedIn = false;
+			if (checkIfUserExists(username)) {
+				isLoggedIn = loginHandler.login(username.toString(), password.toString());
+				if (isLoggedIn) {
+					cookie = createCookie(userService.findByName(username).get(0).isSuperUser());
+				}
+			}
+			if(!isLoggedIn) {
+				handleFailedLoginTry(username);
+			}
+		} else {
+			throw new IllegalAccessError();
+		}
+		return cookie;
+		
+		//return createCookie(userService.findByName(username).get(0).isSuperUser());
+	}
+
+	private boolean isAllowedToTryToLogin(String username) {
+		return !((this.failedLogins.get(username) != null) && (this.failedLogins.get(username) >= MAX_LOGIN_FAILS));
+	}
+
+	private void handleFailedLoginTry(String username) {
+		int nbrOfFaildLogins = 1;
+		if(this.failedLogins.get(username) != null) {
+			nbrOfFaildLogins = this.failedLogins.get(username) + 1;
+		}
+		this.failedLogins.put(username, nbrOfFaildLogins);
+		System.out.println("nbr of faild i hadle: " + nbrOfFaildLogins);
 	}
 
 	public void logout(Cookie[] cookies) {
@@ -56,5 +85,14 @@ public class LoginService {
 		}
 		return cookie;
 	}
-
+	
+	/**
+	 * Removes all the failed login tries every 5 minutes.
+	 * 
+	 * */
+	 
+	@Scheduled(cron = "0 */5 * * * *")
+	public void removeOldLoginTries() {
+		failedLogins = new HashMap<String, Integer>();
+	}
 }
