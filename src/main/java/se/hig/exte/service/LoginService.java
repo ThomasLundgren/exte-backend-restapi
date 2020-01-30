07b4c4b611304ada67f8e9c34d1c837e0bbc1ce8
NiklasNordgren <39832486+NiklasNordgren@users.noreply.github.com>
2020-01-30 17:01:29 +0100

    Update

--- src/main/java/se/hig/exte/service/LoginService.java
+++ src/main/java/se/hig/exte/service/LoginService.java
@@ -36,22 +36,22 @@ public class LoginService {
 	public ResponseCookie login(String username, String password) throws IllegalAccessException {
 		ResponseCookie cookie = null;
 		System.out.println("Service");
-		// if (isAllowedToTryToLogin(username)) {
-		boolean isLoggedIn = false;
-		if (checkIfUserExists(username)) {
-			isLoggedIn = loginHandler.login(username.toString(), password.toString());
-			if (isLoggedIn) {
-				System.out.println("isLoggedIn");
-				cookie = createCookie(userService.findByName(username).get(0).isSuperUser());
-				System.out.println("Cookie: " + cookie);
+		if (isAllowedToTryToLogin(username)) {
+			boolean isLoggedIn = false;
+			if (checkIfUserExists(username)) {
+				isLoggedIn = loginHandler.login(username.toString(), password.toString());
+				if (isLoggedIn) {
+					System.out.println("isLoggedIn");
+					cookie = createCookie(userService.findByName(username).get(0).isSuperUser());
+					System.out.println("Cookie: " + cookie);
+				}
 			}
+			if (!isLoggedIn) {
+				handleFailedLoginTry(username);
+			}
+		} else {
+			throw new IllegalAccessException();
 		}
-		// if (!isLoggedIn) {
-		// handleFailedLoginTry(username);
-		// }
-		// } else {
-		// throw new IllegalAccessException();
-		// }
 		return cookie;
 
 		// return createCookie(userService.findByName(username).get(0).isSuperUser());
