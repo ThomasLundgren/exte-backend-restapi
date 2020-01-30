4a8dea7344be634a88861ab6956f7c189ffa6f27
Thomas Lundgren <thomasLundgren@live.com>
2020-01-30 17:22:50 +0100

    Validation

--- src/main/java/se/hig/exte/model/Course.java
+++ src/main/java/se/hig/exte/model/Course.java
@@ -24,7 +24,7 @@ public class Course {
 	private String name;
 	@NotBlank(message = "Course code cannot be blank")
 	@Size(min = 6, max = 7, message = "Course code must be between six and seven characters long")
-	@Pattern(regexp = "^[A-ZÅÄÖ]{3}[0-9]{3}]", message = "Course code must start with three uppercase letters followed by three or four digits")
+	@Pattern(regexp = "^([A-ZÅÄÖ]{3}[0-9]{3})([A-ZÅÄÖ])?", message = "Course code must start with three uppercase letters followed by three digits and ending with an optional uppercase letter")
 	private String courseCode;
 	private boolean unpublished;
 	private int subjectId;
