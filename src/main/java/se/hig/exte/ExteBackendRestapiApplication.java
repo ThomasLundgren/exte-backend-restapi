ac139bc83720f3e4e033b88cbfa6789eff70ee19
NiklasNordgren <39832486+NiklasNordgren@users.noreply.github.com>
2020-02-10 12:51:14 +0100

    Update

--- src/main/java/se/hig/exte/ExteBackendRestapiApplication.java
+++ src/main/java/se/hig/exte/ExteBackendRestapiApplication.java
@@ -2,9 +2,10 @@ package se.hig.exte;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication(scanBasePackages = { "se.hig.exte" })
+@SpringBootApplication(scanBasePackages = { "se.hig.exte" }, exclude = { HibernateJpaAutoConfiguration.class })
 @EnableScheduling
 public class ExteBackendRestapiApplication {
 
