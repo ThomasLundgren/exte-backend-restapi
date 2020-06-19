3dc649cb1a53e220a1729fee0c7f8a1611223125
MeHasDagger <39961892+MeHasDagger@users.noreply.github.com>
2020-06-19 13:24:06 +0200

    Update ExamController.java

--- src/main/java/se/hig/exte/restcontroller/ExamController.java
+++ src/main/java/se/hig/exte/restcontroller/ExamController.java
@@ -106,7 +106,6 @@ public class ExamController {
 	 */
 	@GetMapping("/all")
 	public ResponseEntity<List<Exam>> getAllExams() {
-		System.out.println("examssss");
 		return new ResponseEntity<List<Exam>>(examService.findAllPublished(), HttpStatus.OK);
 	}
 
