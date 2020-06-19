a2600a2be5f3474777254d70e536e23784dc7063
MeHasDagger <39961892+MeHasDagger@users.noreply.github.com>
2020-06-19 13:30:20 +0200

    Update createJar.bat

--- createJar.bat
+++ createJar.bat
@@ -1,4 +1,4 @@
-DEL "D:\Program Files (x86)\EclipseWorkspace\exte-backend-restapi\build\libs\*.jar"
+DEL "%~dp0\build\libs\*.jar"
 ECHO Building build\libs\ExteBackendApplication-0.1.0.jar
 CALL gradle build
 pause
